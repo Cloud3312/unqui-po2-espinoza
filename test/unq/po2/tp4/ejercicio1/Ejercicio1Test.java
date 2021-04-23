a25bf9b3ae7badb72a2332f7a1ca62c84b10169d
Braian Espinoza <braian3000@gmail.com>
2021-04-22 22:04:03 -0300

    Tp3 agregado

---
+++ test/unq/po2/tp4/ejercicio1/Ejercicio1Test.java
@@ -0,0 +1,24 @@
+package unq.po2.tp4.ejercicio1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class Ejercicio1Test {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
